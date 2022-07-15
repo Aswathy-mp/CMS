36a7a7d86c2f6c0e77362923e102534c83abc3ea
Aswathy-mp <aswathymp24@gmail.com>
2022-07-15 09:23:12 +0530

    signup- validation

--- Frontend/src/app/sign-up/sign-up.component.ts
+++ Frontend/src/app/sign-up/sign-up.component.ts
@@ -8,7 +8,13 @@ import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms'
 })
 export class SignUpComponent implements OnInit {
 
-  constructor() { }
+  constructor(public fb:FormBuilder) { }
+  signupform=this.fb.group({
+    name:new FormControl('',[Validators.required,Validators.pattern(/^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$/)]),
+    email:new FormControl('',[Validators.required,Validators.pattern(/^[a-z0-9.%+]+@[a-z0-9.-]+\.[a-z]{2,4}$/)]),
+    userrole:new FormControl('user',Validators.required),
+    password:new FormControl('',[Validators.required,Validators.pattern(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}$/)])
+  })
 
   ngOnInit(): void {
   }
@@ -17,6 +23,7 @@ export class SignUpComponent implements OnInit {
   }
 
   SignUp(){
-    
+    let signup=this.signupform.value
+    console.log(signup)
   }
 }
